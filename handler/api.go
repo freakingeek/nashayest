d62c963dc733a466241e8705c0a11e5cb5677404
Yasin Silavi <ys.silavi@gmail.com>
2022-06-29 23:40:44 +0430

    refactor: get the illegal words from database

--- handler/api.go
+++ handler/api.go
@@ -11,14 +11,17 @@ import (
 func CheckIllegalWords(c *fiber.Ctx) error {
 	text := c.Query("text")
 	illegalWords := []string{}
-	allTheIllegalWords := []string{"عوضی", "بیشعور"} // Just some random words to test the result
+
+	// Get the illegal words from database
+	allTheIllegalWords := []models.Word{}
+	database.Database.Find(&allTheIllegalWords)
 
 	if text != "" {
 		words := strings.Split(text, " ")
 
 		for _, word := range words {
 			for _, illegal := range allTheIllegalWords {
-				if illegal == word {
+				if illegal.Title == word {
 					illegalWords = append(illegalWords, word)
 				}
 			}
